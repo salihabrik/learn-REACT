ee5ca905b0721c49378e5a317c5363bbcc9ca95c
Software Engineering Student <briksaliha@gmail.com>
2024-05-17 23:57:51 +0200

    Fix footer

--- FOOTER/footer/src/App.js
+++ FOOTER/footer/src/App.js
@@ -54,7 +54,7 @@ const App = () => {
 			
 			<div className="space-y-3">
         
-				<div className="uppercase dark:text-gray-900">Social media</div>
+			
 				<div className="flex justify-start space-x-3">
 					<a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">

2949fe4cce58045ace442df0b04b11115139660c
Software Engineering Student <briksaliha@gmail.com>
2024-06-02 00:32:19 +0200

    update

--- Portfolio/src/components/hero/Hero.jsx
+++ Portfolio/src/components/hero/Hero.jsx
@@ -1,41 +1,69 @@
 import './Hero.scss'
-
+import {motion} from 'framer-motion'
 
 const textVariants = {
   initial: {
     x: -500,
     opacity: 0
-},
-animate: {
-  x: 0,
-  opacity: 1,
-  transition: {
-    duration: 1,
-    staggerChildren: 0.5,
   },
-},
-  };
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const buttonVariants = {
+  initial: {
+    y: 10,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 2,
+    },
+  },
+  scrollButton: {
+    y: 10,
+    opacity: 0,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  }
+};
+
 const Hero = () => {
   return (
     <div className='hero'>
       <div className="wrapper">
-      <div className="textContainer">
-        <h2>Hi, I&apos;m  Saliha  Brik.</h2>
-        <h1> I&apos;m a Frontend Developer.</h1>
-        <p> I&apos;m a Frontend Developer based in Algeria. I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.</p>
-        <div className='buttons'>
-          <button >See the latest projects</button>
-          <button >Contact me</button>
-        </div>
-        <img src="./scroll.png" alt="" />
-    </div>
+        <motion.div 
+          className="textContainer" 
+          variants={textVariants}
+          initial="initial" 
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Hi, I&apos;m Saliha Brik.</motion.h2>
+          <motion.h1 variants={textVariants}>I&apos;m a Frontend Developer.</motion.h1>
+          <motion.p variants={textVariants}>I&apos;m a Frontend Developer based in Algeria. I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.</motion.p>
+          <motion.div className='buttons'>
+            <motion.button variants={buttonVariants}>See the latest projects</motion.button>
+            <motion.button variants={buttonVariants}>Contact me</motion.button>
+          </motion.div>
+          <motion.img variants={textVariants} animate="scrollButton" src="./scroll.png" alt="" />
+        </motion.div>
       </div>
       <div className='slidingTextContainer'>
-        Designer and BAckend Developer
+        Designer and Backend Developer
       </div>
-        <div className="imageContainer">
-          <img src="./hero.jpg" alt="" />
-        </div>
+      <div className="imageContainer">
+        <img src="./hero.jpg" alt="" />
+      </div>
     </div>
   )
 }
